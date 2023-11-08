85fbcf5b4c86c574a7b4c0b7bb274ccbf1be6bd5
Leandro Gualip <752427@student.dusd.net>
2023-11-08 13:46:37 -0800

    Added Health Kit
    Added Health Kit and currently working on script for health kit.

--- RubysAdventureP5Gualip/Assets/Scripts/RubyController.cs
+++ RubysAdventureP5Gualip/Assets/Scripts/RubyController.cs
@@ -5,6 +5,11 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -13,6 +18,9 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -29,5 +37,12 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
     }
 }
